c8fa7ba3d73dee904caa488c88c63a0d0f03ee96
waffleonwaffle <anavmehta1@gmail.com>
2023-04-10 15:12:09 -0700

    Finished 1.1 and 1.2 exercise

---
+++ part1/course-information/src/Content.js
@@ -0,0 +1,12 @@
+import Part from "./Part"
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3 }) => {
+    return (
+        <section>
+            <Part part={part1} exercise={exercises1}/>
+            <Part part={part2} exercise={exercises2}/>
+            <Part part={part3} exercise={exercises3}/>
+        </section>
+
+    )
+}
+export default Content
---
+++ part1/course-information/src/Header.js
@@ -0,0 +1,10 @@
+const Header = ({course}) => {
+
+    return (
+        <h1>
+            {course}
+        </h1>
+    )
+}
+
+export default Header
