af5b6caea2709da9564f13d7e042701ddc759eb5
waffleonwaffle <anavmehta1@gmail.com>
2023-04-11 03:21:17 -0700

    Added key attribute to the mapping of parts in exercise 2.5

--- part2/course-information/src/Content.js
+++ part2/course-information/src/Content.js
@@ -1,7 +1,7 @@
 import Part from './Part' 
 const Content = ({ parts }) => {
     return ( <div>
-        {parts.map(part => <Part part={part} />)}
+        {parts.map(part => <Part key={part.id} part={part} />)}
     </div>
     )
 }
