4ee568379db14f64b55f4baa305819c969cd9ab7
waffleonwaffle <anavmehta1@gmail.com>
2023-04-11 04:12:51 -0700

    Finished exercise 2.8

--- part2/phone-book/src/App.js
+++ part2/phone-book/src/App.js
@@ -1,37 +1,42 @@
 import { useState } from 'react'
-import uniqid from 'uniqid'; 
-const Person = ({person}) => {
+import uniqid from 'uniqid';
+const Person = ({ person }) => {
   return (
     <div>
-      {person.name}
+      {person.name} {person.number}
     </div>
   )
 }
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: uniqid()}
+    { name: 'Arto Hellas', number: '408-996-7867', id: uniqid(), }
   ])
   const [newName, setNewName] = useState('')
-
+  const [newNumber, setNewNumber] = useState('')
   const handleAddPerson = (event) => {
-
     event.preventDefault()
-    console.log(newName)
-    if(persons.some(person => person.name === newName)) {
+    if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
       setNewName('')
+      setNewNumber('')
       return
     }
-    const newNameObject = {
+    const newPerson = {
       name: newName,
+      number: newNumber,
       id: uniqid()
     }
-    setPersons(persons.concat(newNameObject))
+    setPersons(persons.concat(newPerson))
     setNewName('')
-
+    setNewNumber('')
   }
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+
   }
   return (
     <div>
@@ -41,13 +46,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         <div>
-          {persons.map(person => <Person key={person.id} person={person}/>)}
+          {persons.map(person => <Person key={person.id} person={person} />)}
         </div>
 
       </div>
