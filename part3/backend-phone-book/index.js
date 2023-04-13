9c402257e574d45ef0321329ee448e9276b2da20
waffleonwaffle <anavmehta1@gmail.com>
2023-04-13 02:33:29 -0700

    Finished exercise 3.6

--- part3/backend-phone-book/index.js
+++ part3/backend-phone-book/index.js
@@ -61,6 +61,14 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'name missing'
         })
+    } else if(!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    } else if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
     }
     const person = {
         id: generateId(),
