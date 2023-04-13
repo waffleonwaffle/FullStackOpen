9764d783475030c725bf4d096e7652831419095c
waffleonwaffle <anavmehta1@gmail.com>
2023-04-13 02:26:24 -0700

    Finished exercise 3.5

--- part3/backend-phone-book/index.js
+++ part3/backend-phone-book/index.js
@@ -1,5 +1,8 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
+
 let persons = [
     {
         "id": 1,
@@ -47,6 +50,25 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(404).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 100000)
+}
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if(!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number || ''
+    }
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
