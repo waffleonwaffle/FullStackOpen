251fe78e3e5cc896637f8ffa7e50ba471796f442
waffleonwaffle <anavmehta1@gmail.com>
2023-04-13 01:56:04 -0700

    Finished exercise 3.3

--- part3/backend-phone-book/index.js
+++ part3/backend-phone-book/index.js
@@ -33,6 +33,19 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people<p> ${now}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    console.log(id)
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
