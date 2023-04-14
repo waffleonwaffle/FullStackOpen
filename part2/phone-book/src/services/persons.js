629cf37a150f68a57c2152d75aee90eaace99e80
waffleonwaffle <anavmehta1@gmail.com>
2023-04-13 18:57:54 -0700

    Update2 exercise 3.11

--- part2/phone-book/src/services/persons.js
+++ part2/phone-book/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '/api/persons'
+const baseUrl = 'https://localhost:3001/api/persons'
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
