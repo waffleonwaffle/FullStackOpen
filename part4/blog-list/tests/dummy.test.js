057cb696e8742fe4b8ce138e5be5d13904f4b0d3
waffleonwaffle <anavmehta1@gmail.com>
2023-04-17 03:15:22 -0700

    Finished exercise 4.4

--- part4/blog-list/tests/dummy.test.js
+++ part4/blog-list/tests/dummy.test.js
@@ -4,3 +4,61 @@ test('dummy returns one', () => {
     const blogs = []
     expect(list_helper.dummy(blogs)).toBe(1)
 })
+
+test('no blogposts equals 0 likes', () => {
+    const blogs = []
+    expect(list_helper.totalLikes(blogs)).toBe(0)
+})
+
+describe('total likes', () => {
+    const oneBlogPost = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const blogs = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          author: "Michael Chan",
+          url: "https://reactpatterns.com/",
+          likes: 7,
+          __v: 0
+        },
+        {
+          _id: "5a422aa71b54a676234d17f8",
+          title: "Go To Statement Considered Harmful",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: "5a422b3a1b54a676234d17f9",
+          title: "Canonical string reduction",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+          likes: 12,
+          __v: 0
+        },
+      ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+        expect(list_helper.totalLikes(oneBlogPost)).toBe(5)
+    })
+
+    test('when list has multiple blogs, equals the likes of all the blogs', () => {
+        expect(list_helper.totalLikes(blogs)).toBe(24)
+    })
+
+    test('no blogposts equals 0 likes', () => {
+        const blogs = []
+        expect(list_helper.totalLikes(blogs)).toBe(0)
+    })
+})
