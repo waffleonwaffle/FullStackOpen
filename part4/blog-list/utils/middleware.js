e3e081614571547bccebfdeb01c322f03e9be899
waffleonwaffle <anavmehta1@gmail.com>
2023-04-19 00:29:59 -0700

    Finished exercise 4.20

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -16,18 +16,28 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).json({ error: error.message })
     } else if (error.name === 'JsonWebTokenError') {
         return response.status(400).json({ error: error.message })
-    } else if(error.name === 'TokenExpiredError') {
+    } else if (error.name === 'TokenExpiredError') {
         return response.status(400).json({ error: 'token expired' })
     }
     next(error)
 }
 
+
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        request.token = authorization.replace('Bearer ', '')
+    }
+    next()
+}
+
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 module.exports = {
-    requestLogger, 
-    errorHandler, 
-    unknownEndpoint
+    requestLogger,
+    errorHandler,
+    unknownEndpoint, 
+    tokenExtractor
 }
